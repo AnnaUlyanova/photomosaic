26c3d0c61ac1cf2f2311d788a2bb621b4ec378f7
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-03-01 22:59:43 +1300

    getAvRGB test

--- test/tape-test.js
+++ test/tape-test.js
@@ -2,8 +2,10 @@ var test = require('tape')
 var getAverageRGB = require('../js/client.js')
 
 test('should return an object', function (t) {
-  t.getAverageRGB(function () {
-    const rgb = {r: 0, g: 0, b: 0}
-  })
+  var expected = {r: 0, g: 0, b: 0}
+  var actual = function getAverageRGB() {
+    return {r: 0, g: 0, b: 0}
+  }
+  t.equal(actual, expected)
   t.end()
 })
